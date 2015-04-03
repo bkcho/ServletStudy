c14de0e029289eba42a922b5dde032768cddec95
bkcho <ppusarida@gmail.com>
2015-04-03 13:26:45 +0900

    servlet get 방식으로 호출될 때 처리되는 예

--- src/com/servletStudy/myServlet.java
+++ src/com/servletStudy/myServlet.java
@@ -1,27 +1,39 @@
 package com.servletStudy;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/mySvr") // http://localhost:8080/MyServlet/mySvr
+@WebServlet("/mySvr") // http://localhost:8080/ServletStudy/mySvr
 public class myServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
     public myServlet() {
     	 System.out.println("myServlet");     
     }
-
  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doGet");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		 		
+		PrintWriter pw = response.getWriter();
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("Hello World~~~ æ»≥Á«œººø‰~~");
+		pw.println("</body>");
+		pw.println("</html>");
+		pw.close();
 	}
  
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("myServlet");
 	}
-
 }
