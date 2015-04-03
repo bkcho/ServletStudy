1815fb6ca0de4ebfbf79c1ca57ee548c41b180ba
bkcho <ppusarida@gmail.com>
2015-04-03 13:10:34 +0900

    에노테이션 추가. ( URI 주소를 짧게 합축해주는 기능 )

--- src/com/servletStudy/myServlet.java
+++ src/com/servletStudy/myServlet.java
@@ -7,32 +7,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class myServlet
- */
-@WebServlet("/myServlet")
+@WebServlet("/mySvr") // http://localhost:8080/MyServlet/mySvr
 public class myServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
+    
     public myServlet() {
-        // TODO Auto-generated constructor stub
+    	 System.out.println("myServlet");     
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("doGet");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+ 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("myServlet");
 	}
 
 }
