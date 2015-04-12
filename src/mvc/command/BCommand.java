e216f2f0b44c00f153eca696ebe47e7c0c468315
병국 <ppusarida@gmail.com>
2015-04-12 09:46:59 +0900

    mvc패턴 삭제

--- src/mvc/command/BCommand.java
+++
@@ -1,8 +0,0 @@
-package mvc.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BCommand {
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
